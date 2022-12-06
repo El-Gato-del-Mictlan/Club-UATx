68f6e277914563c24f5195d8316144b2f96c5d7f
César Martínez <117472233+El-Gato-del-Mictlan@users.noreply.github.com>
2022-12-06 12:32:50 -0600

    Update Jugador.java

--- Herencia/Jugador.java
+++ Herencia/Jugador.java
@@ -1,13 +1,17 @@
-public class Jugador extends Plantel{
+public class Jugador extends Plantel{ //Para indicar que es una clase hija usamos “extends” seguido del nombre de la clase padre
+    
+    //Declaramos los atributos de nuestra clase
     private String posicion;
     private int numero;
-
+    
+    //Generamos el constructor para inicializar los atributos de nuestra clase.
     public Jugador(String nombre, String apellido, int edad, String posicion, int numero) {
         super(nombre, apellido, edad);
         this.posicion = posicion;
         this.numero = numero;
     }
-
+    
+    //Getter y Setter
     public String getPosicion() {
         return posicion;
     }
@@ -23,7 +27,8 @@ public class Jugador extends Plantel{
     public void setNumero(int numero) {
         this.numero = numero;
     }
-
+    
+    //Sobrescribimos el método toString para mostrar los atributos de nuestro objeto. 
     @Override
     public String toString(){
         return "\nJugador: " + nombre + " " + apellido + "\nNúmero: " + numero + "\nPosición: " + posicion +
