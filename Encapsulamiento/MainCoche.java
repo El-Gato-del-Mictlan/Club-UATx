d93cba406476e1b96cc459e71f88f499790bc5e0
César Martínez <117472233+El-Gato-del-Mictlan@users.noreply.github.com>
2022-12-06 12:22:27 -0600

    Update MainCoche.java

--- Encapsulamiento/MainCoche.java
+++ Encapsulamiento/MainCoche.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //Creación de un objeto de tipo coche.
         Coche coche1 = new Coche("Mitsubishi","Evolution","Morado",5,75000);
-        System.out.println(coche1.getColor());
-        System.out.println(coche1.toString());
+        
+        System.out.println(coche1.getColor()); //Imprimimos el color de nuestro coche1.
+        System.out.println(coche1.toString()); //Con ayuda del método toString imprimimos los atributos de nuestro objeto.
 
-        coche1.setMarca("Subaru");
-        coche1.setModelo("WRX");
-        coche1.setColor("Azul");
-        coche1.setnPuertas(3);
-        coche1.setKilometraje(45000);
-        System.out.println(coche1.toString());
-
+        coche1.setMarca("Subaru"); //Asignamos una nueva marca
+        coche1.setModelo("WRX"); //Asignamos un nuevo módelo
+        coche1.setColor("Azul"); //Asignamos un nuevo color
+        coche1.setnPuertas(3);  //Asignamos un nuevo número de puertas
+        coche1.setKilometraje(45000); //Asignamos un nuevo kilometraje. 
+        System.out.println(coche1.toString()); //Con ayuda del método toString imprimimos los atributos de nuestro objeto.
+        
+        
+        //Accediendo a los atributos de nuestro objeto sin hacer uso del principio de encapsulamiento. 
         /*Coche coche1 = new Coche();
         coche1.marca = "Mitsubishi";
         coche1.modelo = "Evolution";
