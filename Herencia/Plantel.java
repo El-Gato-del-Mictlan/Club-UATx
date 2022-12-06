db987f92eee1846e83eb422b68966f1d37808fdf
César Martínez <117472233+El-Gato-del-Mictlan@users.noreply.github.com>
2022-12-06 12:30:58 -0600

    Update Plantel.java

--- Herencia/Plantel.java
+++ Herencia/Plantel.java
@@ -1,20 +1,26 @@
 public class Plantel {
+    
+    //Declaramos los atributos de nuestra clase 
+    //Usamos protected para que la clase hija pueda acceder a los atributos de la clase padre sin la necesidad de usar Get o Set.
     protected String nombre, apellido, cargo;
     protected int edad;
-
+    
+    //Generamos el constructor para inicializar los atributos de nuestra clase.
     public Plantel(String nombre, String apellido, String cargo, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.cargo = cargo;
         this.edad = edad;
     }
-
+    
+    //Generamos el constructor para crear objetos de la clase hija (jugador), es uno nuevo porque no le pasamos un atributo.
     public Plantel(String nombre, String apellido, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
     }
-
+    
+    //Getter y Setter
     public String getNombre() {
         return nombre;
     }
@@ -46,7 +52,8 @@ public class Plantel {
     public void setEdad(int edad) {
         this.edad = edad;
     }
-
+    
+    
     @Override
     public String toString(){
         return "\nCuerpo técnico: \nCargo: " + cargo + "\n" + nombre + " " + apellido + "\nEdad: " + edad + " años";
