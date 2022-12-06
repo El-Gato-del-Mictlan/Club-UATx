2f8f5ef6947dc948a8280dbf93b9506a82ff5194
César Martínez <117472233+El-Gato-del-Mictlan@users.noreply.github.com>
2022-12-06 12:46:31 -0600

    Update MainPlantel.java

--- Herencia/MainPlantel.java
+++ Herencia/MainPlantel.java
@@ -1,53 +1,62 @@
+//Importamos las librerías necesarias 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainPlantel {
+    
+    //Al declarar objetos fuera del main estos deben ser estáticos. 
+    //Usamos ArrayList porque no vamos a definirle un tamaño fijo a nuestro array. 
+    static ArrayList<Plantel> CuerpoTecnico = new ArrayList(); //Para Plantel
+    static ArrayList<Jugador> equipo = new ArrayList(); //Para Jugadores
 
-    static ArrayList<Plantel> CuerpoTecnico = new ArrayList();
-    static ArrayList<Jugador> equipo = new ArrayList();
-
-    static Scanner sc = new Scanner(System.in);
-
+    static Scanner sc = new Scanner(System.in); //Declaración de Scanner
+    
     public static void main(String[] args) {
-        ingresarEquipo();
-        mostarDT();
-        mostrarJugadores();
+        ingresarEquipo(); //Metódo para ingresar algún jugador o directivo. 
+        mostarDT(); //Método para mostrar a la directiva.
+        mostrarJugadores(); //Método para mostrar jugadores.
     }
-
+    
+    //Metódo para ingresar algún jugador o directivo. 
     public static void ingresarEquipo(){
-        int tipo;
-        boolean dtAsignado = false;
-
+        int tipo; //Auxiliar para definir que se va a ingresar (directivo o jugador)
+        boolean dtAsignado = false; //Bandera para saber si el DT ya fue asignado. 
+        
+        //Ciclos para llenado de datos
         do {
 
             do {
-                System.out.println("1-> Asignar DT  2->Fichar Jugador 0->Fin");
-                tipo = sc.nextInt();
-            }while (tipo < 0 || tipo > 2);
+                System.out.println("1-> Asignar DT  2->Fichar Jugador 0->Fin"); //Solicitamos la opción a realizar
+                tipo = sc.nextInt(); //Se lee la opción
+            }while (tipo < 0 || tipo > 2); //Se hace la comparación para ver que sea un valor valido
 
-            if (tipo != 0){
-                switch (tipo){
-                    case 1:
-                        if (dtAsignado == true)
-                            System.out.println("Ya asignaste un DT");
+            if (tipo != 0){ //Si se seleccionó una opción valida ingresará a la sentencia
+                switch (tipo){ //Se compara la variable para asignarle un caso
+                    case 1: //Asignar DT
+                        if (dtAsignado == true) //Se compara si es que ya se asigno un director técnico
+                            System.out.println("Ya asignaste un DT"); //Mensaje de que ya fue asignado
                         else {
-                            ingresarTecnico();
-                            dtAsignado = true;
+                            ingresarTecnico(); //Se llama al metodo para llenar los datos correspondientes
+                            dtAsignado = true; //Se cambia la bandera a verdadero 
                         }
-                        break;
+                        break; //break para que se detenga en switch
                     case 2:
-                        ingresarJugadores();
-                        break;
+                        ingresarJugadores(); //Se llama al metodo para llenar los datos correspondientes
+                        break; //break para que se detenga en switch
                 }
             }
-        }while (tipo != 0);
+        }while (tipo != 0); //Se compara que no se haya solicitado fin del programa
     }
-
+    
+    //Método para ingresar a la directiva  
     public static void ingresarTecnico(){
+        //Declaración de variables auxiliares para llenar los datos 
         String nombre, apellido, cargo;
         int edad;
 
-        sc.nextLine();
+        sc.nextLine(); //Se limpia el scanner para evitar errores
+        
+        //Solicitud de datos
         System.out.println("Ingresa el nombre: ");
         nombre = sc.nextLine();
         System.out.println("Ingresa el apelllido: ");
@@ -56,40 +65,56 @@ public class MainPlantel {
         cargo = sc.nextLine();
         System.out.println("Ingresa la edad: ");
         edad = sc.nextInt();
-
+        
+        //Se crea un objeto de tipo plantrel y le pasamos como argumentos lo que está en nuestras variables auxiliares.
         Plantel staff = new Plantel(nombre,apellido,cargo,edad);
+        //Añadimos el objeto staff a nuestra ArrayList correspondiente 
         CuerpoTecnico.add(staff);
     }
-
+    
+    //Método para ingresar a los jugadores  
     public static void ingresarJugadores(){
+        //Declaración de variables auxiliares para llenar los datos 
         String nombre, apellido, posicion;
         int edad, num;
 
-        sc.nextLine();
+        sc.nextLine(); //Se limpia el scanner para evitar errores
+        
+        //Solicitud de datos
         System.out.println("Ingresa el nombre: ");
         nombre = sc.nextLine();
         System.out.println("Ingresa el apelllido: ");
         apellido = sc.nextLine();
         System.out.println("Ingresa la edad: ");
         edad = sc.nextInt();
-        sc.nextLine();
+        
+        sc.nextLine(); //Se limpia el scanner para evitar errores
+        
         System.out.println("Ingresa la posición: ");
         posicion = sc.nextLine();
         System.out.println("Ingresa el número: ");
         num = sc.nextInt();
-
+        
+        //Se crea un objeto de tipo jugador y le pasamos como argumentos lo que está en nuestras variables auxiliares.
         Jugador jugador = new Jugador(nombre,apellido,edad,posicion,num);
+        //Añadimos el objeto equipo a nuestra ArrayList correspondiente 
         equipo.add(jugador);
     }
-
+    
+    //Método para mostrar a la directiva.
     public static void mostarDT(){
+        //Recorremos el ArrayList con ayuda de un Forech
         for (Plantel d:CuerpoTecnico){
+            //La variable d es la que va a recorrer nuestro Array e imprimimos los datos con ayuda del método toString
             System.out.println(d.toString());
         }
     }
-
+    
+    //Método para mostrar jugadores.
     public static void mostrarJugadores(){
+        //Recorremos el ArrayList con ayuda de un Forech
         for (Jugador j:equipo){
+            //La variable j es la que va a recorrer nuestro Array e imprimimos los datos con ayuda del método toString
             System.out.println(j.toString());
         }
     }
